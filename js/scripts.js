a5abe25406c909a2925c654bf36c0bb6c2aa74ff
Winnie Shen <winnieshenny@gmail.com>
2021-03-26 17:43:08 -0400

    final edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//Intro overlay from w3schools
+//Intro overlay
 
 function openIntro() {
   document.getElementById("myIntro").style.height = "100%";
@@ -9,6 +9,14 @@ function closeIntro() {
   document.getElementById("myIntro").style.height = "0%";
   document.querySelector('body').style.overflow = 'auto';
 }
+
+// load final data initially so page doesn't lag when
+// selecting neighborhoods
+var MCI_FINAL_DATA = [];
+$.getJSON("./data/mci-final-v4.geojson", function(data) {
+  MCI_FINAL_DATA = data
+})
+
 
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoid3NoZW55YyIsImEiOiJja2w3YjNvd3YxZnc1Mm5wZWp1MnVqZGh2In0.-wG4LWFGN76Nf-AEigxu2A';
@@ -50,7 +58,6 @@ map.on('load', function() {
     }
   });
 
-
   // adding in layer of just lots with MCIs
 
   map.addSource('mcilots', {
@@ -80,89 +87,19 @@ map.on('load', function() {
 
 });
 
-//filter map by NTA
-function set_nta() {
-  var select = document.getElementById("nta_code");
-  var nta = select.options[select.selectedIndex].value;
-
-  map.setFilter('manhattan-mci-lots',
-    ['==', ['get', 'nta'], nta]
-  )
-
-  map.setFilter('mci-outlines',
-    ['==', ['get', 'nta'], nta]
-  )
-
-  map.setFilter('lots-outlines',
-    ['==', ['get', 'NTACode'], nta]
-  )
-
-
-  //going to add a zoom to NTA here
-
-
-  $.getJSON("./data/nta-nyc.geojson", function(data) {
-      var neighborhood = data.features.find(function(feature) {
-        return feature.properties.NTACode === nta;
-      });
-      // nta data is formatted inconsistently, so ensure it's
-      // broken down to the format [[lng, lat], [lng, lat]]
-      var coords = flatten(neighborhood.geometry.coordinates)
-
-      var turfFeatures = turf.points(coords);
-      var newCenter = turf.center(turfFeatures);
-      map.flyTo({
-        center: newCenter.geometry.coordinates,
-        zoom: 14,
-        speed: 1
-      })
-    })
-}
-
-
-
-
-//reset map
-function resetMap() {
-  map.setFilter('manhattan-mci-lots')
-
-  map.setFilter('mci-outlines')
-
-  map.setFilter('lots-outlines',
-    ['==', 'BoroName', 'Manhattan'])
-
-  map.easeTo({
-    center: [-73.992075979463, 40.7367347085187], // starting position [lng, lat]
-    zoom: 13.5 // starting zoom
-  })
-}
-
-// zoom the map to the selected features
-
-
-
 //Target the span elements used in the sidebar
 var addDisplay = document.getElementById('address');
-var itemDisplay = document.getElementById('item');
-var amountDisplay = document.getElementById('mciamount');
-var allowCostDisplay = document.getElementById('allowcost')
-var statusDisplay = document.getElementById('finaldecision');
-var dateDisplay = document.getElementById('mcidate');
-var closingDateDisplay = document.getElementById('closingdate');
-var infoContainer = document.getElementById('infoContainer');
-
-
-var buildingID = null;
 
 map.on('click', 'manhattan-mci-lots', function(e) {
   // Set variables equal to the current feature's address, MCI item, claim cost, filing date
   var address = e.features[0].properties.full_address;
 
-
   if (e.features.length > 0) {
     // Display the address, MCI item, claim cost, filing date in the sidebar
     var MCIElements = e.features.map((feature, idx) => {
-      var { properties } = feature;
+      var {
+        properties
+      } = feature;
       return `<div>
         <h3 class="mciItemHeader" style="${idx === 0 ? 'padding-top:0' : ''}">
           ${properties.mci_item.toLowerCase() === 'na' ? 'Item Unknown' : properties.mci_item}
@@ -187,11 +124,134 @@ map.on('click', 'manhattan-mci-lots', function(e) {
     });
 
     addDisplay.textContent = address;
-    infoContainer.innerHTML =  MCIElements.join('');
+    infoContainer.innerHTML = MCIElements.join('');
   }
 
 });
 
+//filter map by NTA
+function set_nta() {
+  var select = document.getElementById("nta_code");
+  var nta = select.options[select.selectedIndex].value;
+
+  map.setFilter('manhattan-mci-lots',
+    ['==', ['get', 'nta'], nta]
+  )
+
+  map.setFilter('mci-outlines',
+    ['==', ['get', 'nta'], nta]
+  )
+
+  map.setFilter('lots-outlines',
+    ['==', ['get', 'NTACode'], nta]
+  )
+
+  getAndDisplayMCIAverages(MCI_FINAL_DATA, nta)
+
+  //Zoom to selected NTA
+  $.getJSON("./data/nta-nyc.geojson", function(data) {
+    var neighborhood = data.features.find(function(feature) {
+      return feature.properties.NTACode === nta;
+    });
+    // nta data is formatted inconsistently, so ensure it's
+    // broken down to the format [[lng, lat], [lng, lat]]
+    var coords = flatten(neighborhood.geometry.coordinates)
+
+    var turfFeatures = turf.points(coords);
+    var newCenter = turf.center(turfFeatures);
+    map.flyTo({
+      center: newCenter.geometry.coordinates,
+      zoom: 14,
+      speed: 1
+    })
+  })
+
+  //display aggregated Data
+
+  document.getElementById("NTA_info").style.visibility = "visible";
+
+  //   var sum = 0;
+  //
+}
+
+// helpers
+
+function getAndDisplayMCIAverages(data, nta) {
+  var nbhood = data.features.filter(function(feature) {
+    return feature.properties.nta === nta;
+  });
+  var totalClaimCost = 0
+  var totalAllowCost = 0
+  var totalClaimCount = 0
+  var totalAllowCount = 0;
+  var mciItems = {}
+  nbhood.forEach(building => {
+    if (!isNaN(parseFloat(building.properties.claim_cost))) {
+      totalClaimCost += parseFloat(building.properties.claim_cost);
+      totalClaimCount += 1
+    }
+    if (!isNaN(parseFloat(building.properties.allow_cost))) {
+      totalAllowCost += parseFloat(building.properties.allow_cost);
+      totalAllowCount += 1
+    }
+
+    if (building.properties.mci_item) {
+      if (mciItems[building.properties.mci_item]) {
+        mciItems[building.properties.mci_item] += 1
+      } else {
+        mciItems[building.properties.mci_item] = 1
+      }
+    }
+  })
+
+  var avgClaimCost = roundNumber(totalClaimCost / totalClaimCount)
+  var avgAllowCost = roundNumber(totalAllowCost / totalAllowCount)
+
+  var mostPopular = '';
+  var highestCount = 0;
+  Object.keys(mciItems).forEach(key => {
+    if (key.toLowerCase() === 'na' || key.toLowerCase() === 'n/a') return
+    if (mciItems[key] > highestCount) {
+      highestCount = mciItems[key]
+      mostPopular = key
+    }
+  })
+  document.getElementById('commonItem').textContent = `${mostPopular} (${highestCount})`
+  document.getElementById('averageClaim').textContent = avgClaimCost;
+  document.getElementById('averageGrant').textContent = avgAllowCost;
+}
+
+function roundNumber(num) {
+  return (Math.round(num * 100) / 100).toFixed(2);
+}
+
+
+//reset map
+function resetMap() {
+
+  //resets layers back to original
+  map.setFilter('manhattan-mci-lots')
+
+  map.setFilter('mci-outlines')
+
+  map.setFilter('lots-outlines',
+    ['==', 'BoroName', 'Manhattan'])
+
+  //removes content in info container
+  addDisplay.textContent = "";
+  infoContainer.innerHTML = "";
+
+  //removes aggregate window
+
+  document.getElementById("NTA_info").style.visibility = "hidden";
+
+  //shifts map back to starting center and zoom level
+  map.easeTo({
+    center: [-73.992075979463, 40.7367347085187], // starting position [lng, lat]
+    zoom: 13.5 // starting zoom
+  })
+
+}
 
 // recursive array flattener, return array of arrays, in geojson formatt,
 // i.e. [[lng, lat], [lng, lat]]
@@ -205,21 +265,32 @@ function flatten(array) {
   array.forEach(el => newArray = [...newArray, ...el])
   return flatten(newArray)
 }
-//filter feature by Address
-// var filterEl = document.getElementById('feature-filter');
-// var listingEl = document.getElementById('feature-listing');
-//
-// filterEl.parentNode.style.display = 'block';
-// }
-// else if (features.length === 0 && filterEl.value !== '') {
-//   empty.textContent = 'No results found';
-//   listingEl.appendChild(empty);
-// } else {
-//   empty.textContent = 'Drag the map to populate results';
-//   listingEl.appendChild(empty);
-//
-//   // Hide the filter input
-//   filterEl.parentNode.style.display = 'none';
+
+// Get the modal
+var modal = document.getElementById("faqModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "visible";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 //Mouse cursor will change to a pointer when over something clickable
